4642e23be1832ff2792ebb95a1f707787c161bd0
Aleksandr <iisyys@gmail.com>
2022-11-09 20:21:07 +0400

    basee

--- prostotit_bot.py
+++ prostotit_bot.py
@@ -6,60 +6,34 @@ from openpyxl import load_workbook
 
 bot = telebot.TeleBot('5579090888:AAGMUTxFCVRXm0UcVWnoy3UkePrImGasK4g')
 
-#установка эксель файла если его нет в папке с ботом
-def setup_xlsx():
+
+def get_name(message): #получаем фамилию
+    global name;
+    name = message.text;
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?');
+    bot.register_next_step_handler(message, get_surname);
+
+def get_surname(message):
+    global surname;
+    surname = message.text;
+    bot.send_message(message.from_user.id,'Сколько тебе лет?');
+    bot.register_next_step_handler(message, get_age);
+
+def get_age(message):
+    global age
     try:
-        open('schedule.xlsx')
+             age = int(message.text)
+             if age > 18:
+                bot.send_message(message.from_user.id,
+                              'Тебе ' + str(age) + ' лет, тебя зовут ' + name + ' ' + surname + '?')
+             else:
+                 bot.send_message(message.from_user.id, 'Ты пиздюк иди нахуй!')
+                 bot.register_next_step_handler(message, get_age)
+
     except Exception:
-        wb = openpyxl.Workbook()
-        wb.save('schedule.xlsx')
-setup_xlsx()
+        bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+        bot.register_next_step_handler(message, get_age)
 
-
-@bot.message_handler(content_types=['photo', 'document'] )
-def get_user_text(message):
-    file_info = bot.get_file(message.document.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-
-    src = 'C:/Users/Александр/Desktop/Александр/боты/prostotit_bot/Photoo/' + message.document.file_id + ".png"
-    with open(src, 'wb') as new_file:
-        new_file.write(downloaded_file)
-
-
-
-
-
-
-
-
-
-# def get_name(message): #получаем фамилию
-#     global name;
-#     name = message.text;
-#     bot.send_message(message.from_user.id, 'Какая у тебя фамилия?');
-#     bot.register_next_step_handler(message, get_surname);
-#
-# def get_surname(message):
-#     global surname;
-#     surname = message.text;
-#     bot.send_message(message.from_user.id,'Сколько тебе лет?');
-#     bot.register_next_step_handler(message, get_age);
-#
-# def get_age(message):
-#     global age
-#     try:
-#              age = int(message.text)
-#              if age > 18:
-#                 bot.send_message(message.from_user.id,
-#                               'Тебе ' + str(age) + ' лет, тебя зовут ' + name + ' ' + surname + '?')
-#              else:
-#                  bot.send_message(message.from_user.id, 'Ты пиздюк иди нахуй!')
-#                  bot.register_next_step_handler(message, get_age)
-#
-#     except Exception:
-#         bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
-#         bot.register_next_step_handler(message, get_age)
-#
 
 
 
