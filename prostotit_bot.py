7b910da19872152c0a08cad0cccf8a68743f917c
Aleksandr <iisyys@gmail.com>
2022-11-11 17:40:19 +0400

    1

--- prostotit_bot.py
+++ prostotit_bot.py
@@ -15,14 +15,13 @@ def setup_xlsx():
 setup_xlsx()
 
 
-@bot.message_handler(content_types=['photo', 'document'] )
+@bot.message_handler(content_types=['photo', 'document'])
 def get_user_text(message):
     file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
     print(file_info)
     file_format_in = message.document.file_name.rfind('.')
     file_format = message.document.file_name[file_format_in:]
-
 
     src = 'C:/Users/Александр/Desktop/Александр/боты/prostotit_bot/Photoo/' + message.document.file_id + file_format
     with open(src, 'wb') as new_file:
@@ -31,10 +30,13 @@ def get_user_text(message):
 
 
 def wright_name(src):
+    n = 1
     a = openpyxl.load_workbook('schedule.xlsx')
     ws = a.active
-    ws['A1'] = src
-    print(ws['A1'].value)
+    while ws[f'A{n}'].value != None:
+        n += 1
+    ws[f'A{n}'] = src
+    print(ws[f'A{n}'].value)
     a.save('schedule.xlsx')
 
 
